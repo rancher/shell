271f6313383b25aa1dbe2cb7c6dfb1b635cce48b
Dan Pock <self@danpock.me>
2025-03-26 09:22:24 -0400

    Update to use new datasource feature

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -5,9 +5,11 @@ KUBECTL_VERSION := v1.31.6
 KUBECTL_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL_VERSION)/bin/linux/arm64/kubectl.sha256")
 KUBECTL_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL_VERSION)/bin/linux/amd64/kubectl.sha256")
 
-# renovate: datasource=github-release-attachments depName=kubernetes-sigs/kustomize extractVersion=kustomize/v(?<version>\d+\.\d+\.\d+)
+# renovate-local: kustomize-amd64
 KUSTOMIZE_VERSION := v5.6.0
+# renovate-local: kustomize-arm64=v5.5.0
 KUSTOMIZE_SUM_arm64 := ad8ab62d4f6d59a8afda0eec4ba2e5cd2f86bf1afeea4b78d06daac945eb0660
+# renovate-local: kustomize-amd64=v5.5.0
 KUSTOMIZE_SUM_amd64 := 54e4031ddc4e7fc59e408da29e7c646e8e57b8088c51b84b3df0864f47b5148f
 
 # renovate: datasource=github-release-attachments depName=derailed/k9s
