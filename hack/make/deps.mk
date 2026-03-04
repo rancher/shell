6df1d1370387e1dd676f78e3d51fed2a23589ef7
Dan Pock <self@danpock.me>
2026-03-04 11:52:57 -0500

    Bump k9s, kubectl and kustomize versions

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -2,25 +2,25 @@
 HELM_VERSION := v3.19.0-rancher1
 
 # renovate-local: kubectl-amd64
-KUBECTL_VERSION := v1.33.1
-# renovate-local: kubectl-arm64=v1.33.1
-KUBECTL_SUM_arm64 := d595d1a26b7444e0beb122e25750ee4524e74414bbde070b672b423139295ce6
-# renovate-local: kubectl-amd64=v1.33.1
-KUBECTL_SUM_amd64 := 5de4e9f2266738fd112b721265a0c1cd7f4e5208b670f811861f699474a100a3
+KUBECTL_VERSION := v1.33.9
+# renovate-local: kubectl-arm64=v1.33.9
+KUBECTL_SUM_arm64 := af4dc943a6f447ecb070340efe63c7f8ee2808e6c0bc42126efe7cde0cc1e69b
+# renovate-local: kubectl-amd64=v1.33.9
+KUBECTL_SUM_amd64 := 9e33e3234c0842cd44a12c13e334b4ce930145ea84b855ce7cc0a7b6bc670c22
 
 # renovate-local: kustomize-amd64
-KUSTOMIZE_VERSION := v5.6.0
-# renovate-local: kustomize-arm64=v5.6.0
-KUSTOMIZE_SUM_arm64 := ad8ab62d4f6d59a8afda0eec4ba2e5cd2f86bf1afeea4b78d06daac945eb0660
-# renovate-local: kustomize-amd64=v5.6.0
-KUSTOMIZE_SUM_amd64 := 54e4031ddc4e7fc59e408da29e7c646e8e57b8088c51b84b3df0864f47b5148f
+KUSTOMIZE_VERSION := v5.7.1
+# renovate-local: kustomize-arm64=v5.7.1
+KUSTOMIZE_SUM_arm64 := 4261a040217df3bd6896597c3986d1465925726e4f22a945304b5233a4dcdbda
+# renovate-local: kustomize-amd64=v5.7.1
+KUSTOMIZE_SUM_amd64 := ea375e7372f9aa029129d4b2d16c66b7750b7f1213c4f66f910d981c895818d8
 
 # renovate: datasource=github-release-attachments depName=derailed/k9s
-K9S_VERSION := v0.50.16
-# renovate: datasource=github-release-attachments depName=derailed/k9s digestVersion=v0.50.16
-K9S_SUM_arm64 := 7f3b414bc5e6b584fbcb97f9f4f5b2c67a51cdffcbccb95adcadbaeab904e98e
-# renovate: datasource=github-release-attachments depName=derailed/k9s digestVersion=v0.50.16
-K9S_SUM_amd64 := bda09dc030a08987fe2b3bed678b15b52f23d6705e872d561932d4ca07db7818
+K9S_VERSION := v0.50.18
+# renovate: datasource=github-release-attachments depName=derailed/k9s digestVersion=v0.50.18
+K9S_SUM_arm64 := d3dcc051d6be26ee911c00f583412802ebe203a189e51bc079332cb410c83b38
+# renovate: datasource=github-release-attachments depName=derailed/k9s digestVersion=v0.50.18
+K9S_SUM_amd64 := 0b697ed4aa80997f7de4deeed6f1fba73df191b28bf691b1f28d2f45fa2a9e9b
 
 # Reduces the code duplication on Makefile by keeping all args into a single variable.
 IMAGE_ARGS := --build-arg HELM_VERSION=$(HELM_VERSION) \
