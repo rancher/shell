836796154a9d431142bdc6f5e51b603365b4b6ef
Rodolfo Mói de Oliveira <rodolfo.oliveira@suse.com>
2026-04-24 10:33:11 -0300

    Remove kustomize (#436)
    Replaces `kustomize` with a minimal shell script that proxies the
    `build` subcommand to `kubectl kustomize` and rejects everything else.
    
    * Remove kustomize.sh script
    * Remove kustomize binary
    * Remove kustomize from helm-cmd
    * Add kustomize wrapper script
    
    See rancher/rancher#54676
    
    Signed-off-by: Rodolfo Mói de Oliveira <rodolfo.oliveira@suse.com>

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -5,13 +5,6 @@ KUBECTL_VERSION := v1.31.7
 KUBECTL_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL_VERSION)/bin/linux/arm64/kubectl.sha256")
 KUBECTL_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL_VERSION)/bin/linux/amd64/kubectl.sha256")
 
-# renovate-local: kustomize-amd64
-KUSTOMIZE_VERSION := v5.6.0
-# renovate-local: kustomize-arm64=v5.6.0
-KUSTOMIZE_SUM_arm64 := ad8ab62d4f6d59a8afda0eec4ba2e5cd2f86bf1afeea4b78d06daac945eb0660
-# renovate-local: kustomize-amd64=v5.6.0
-KUSTOMIZE_SUM_amd64 := 54e4031ddc4e7fc59e408da29e7c646e8e57b8088c51b84b3df0864f47b5148f
-
 # renovate: datasource=github-release-attachments depName=derailed/k9s
 K9S_VERSION := v0.50.0
 # renovate: datasource=github-release-attachments depName=derailed/k9s digestVersion=v0.50.0
@@ -22,5 +15,4 @@ K9S_SUM_amd64 := 63eb225a3da358245d37e7f79904d62c1d5361e4faa9275c43fb3cdbb053dde
 # Reduces the code duplication on Makefile by keeping all args into a single variable.
 IMAGE_ARGS := --build-arg HELM_VERSION=$(HELM_VERSION) \
 			  --build-arg KUBECTL_VERSION=$(KUBECTL_VERSION) --build-arg KUBECTL_SUM_arm64=$(KUBECTL_SUM_arm64) --build-arg KUBECTL_SUM_amd64=$(KUBECTL_SUM_amd64) \
-			  --build-arg KUSTOMIZE_VERSION=$(KUSTOMIZE_VERSION) --build-arg KUSTOMIZE_SUM_arm64=$(KUSTOMIZE_SUM_arm64) --build-arg KUSTOMIZE_SUM_amd64=$(KUSTOMIZE_SUM_amd64) \
 			  --build-arg K9S_VERSION=$(K9S_VERSION) --build-arg K9S_SUM_arm64=$(K9S_SUM_arm64) --build-arg K9S_SUM_amd64=$(K9S_SUM_amd64)
