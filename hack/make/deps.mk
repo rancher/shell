9f5c0fc2bd680d55f462b1d98b3b025290d1a8dd
Alejandro Moreno <alejandro.moreno@suse.com>
2026-09-15 15:28:17 +0200

    chore: bump kubectl version to v1.35.8 (#494)
    * chore: bump kubectl version to v1.35.8
    
    * chore: update hardened-build-base to v1.26.8b1

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -2,7 +2,7 @@
 HELM_VERSION := v3.20.0-rancher2
 
 # renovate-local: kubectl-amd64
-KUBECTL_VERSION := v1.35.5
+KUBECTL_VERSION := v1.35.8
 
 # renovate: datasource=github-release-attachments depName=derailed/k9s
 K9S_VERSION := v0.51.0
