9c26b7015194b038aa8f78e80b67042fba990c92
Abhishek Valaboju <abhishek.valaboju@suse.com>
2026-09-17 14:40:41 +0530

    Bump Helm v4.3.0 bump k8s v1.37 (#488)
    Co-authored-by: Abhishek Valaboju <abhishekvalaboju@Mac.lan>

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -1,7 +1,7 @@
-HELM_VERSION := v4.2.0
+HELM_VERSION := v4.3.0
 
 # renovate-local: kubectl-amd64
-KUBECTL_VERSION := v1.36.1
+KUBECTL_VERSION := v1.37.0
 
 # renovate: datasource=github-release-attachments depName=derailed/k9s
 K9S_VERSION := v0.51.0
